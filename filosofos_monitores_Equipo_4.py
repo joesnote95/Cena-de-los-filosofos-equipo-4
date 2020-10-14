0c39a7ecb76e2cd1b1c5e325e298ee3e918e19b5
José Octavio Echevarría Sánchez <72844909+joesnote95@users.noreply.github.com>
2020-10-13 22:11:58 -0500

    Update filosofos_monitores_Equipo_4.py

--- filosofos_monitores_Equipo_4.py
+++ filosofos_monitores_Equipo_4.py
@@ -97,6 +97,9 @@ def main():
 
     for i in range(n):
         filosofos[i].start()        #Se inicia el proceso con cada uno de los filósofos
+    
+    msvcrt.getch()
+
 
 
 if __name__ == "__main__":
